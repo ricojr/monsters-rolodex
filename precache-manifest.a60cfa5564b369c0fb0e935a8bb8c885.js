e6d83f322e2cec903a453706e1106fcf3ed04a26
ricojr <ricopehangko@gmail.com>
2020-02-10 14:50:43 +0100

    Updates

---
+++ precache-manifest.a60cfa5564b369c0fb0e935a8bb8c885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab7e074af128a29a0f75bb2b9f503ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "11272849f66f5179f913",
+    "url": "/monsters-rolodex/static/css/main.7f26d78e.chunk.css"
+  },
+  {
+    "revision": "b59f44af4d4a1bf76f80",
+    "url": "/monsters-rolodex/static/js/2.215fb3a6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.215fb3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11272849f66f5179f913",
+    "url": "/monsters-rolodex/static/js/main.06347aef.chunk.js"
+  },
+  {
+    "revision": "6da5879867585b6861df",
+    "url": "/monsters-rolodex/static/js/runtime-main.cdf3e700.js"
+  }
+]);
